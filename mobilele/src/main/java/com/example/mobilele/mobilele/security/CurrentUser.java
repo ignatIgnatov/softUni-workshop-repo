33c8020c214529c9cc597910e957d4ec578fe6ca
Ignat Ignatov <ignatov_rs@abv.bg>
2023-06-27 14:50:21 +0300

    hide login button for logged users

--- mobilele/src/main/java/com/example/mobilele/mobilele/security/CurrentUser.java
+++ mobilele/src/main/java/com/example/mobilele/mobilele/security/CurrentUser.java
@@ -10,7 +10,7 @@ public class CurrentUser {
     private static final String ANONYMOUS = "anonymous";
 
     private String name = ANONYMOUS;
-    private boolean isAnonymous;
+    private boolean isAnonymous = true;
 
     public String getName() {
         return name;
@@ -23,6 +23,10 @@ public class CurrentUser {
 
     public boolean isAnonymous() {
         return isAnonymous;
+    }
+
+    public boolean isLoggedIn() {
+        return !isAnonymous;
     }
 
     public CurrentUser setAnonymous(boolean anonymous) {

4bb2931a750b7771c68b236cfc4f3fa41506550d
Ignat Ignatov <ignatov_rs@abv.bg>
2023-06-27 17:03:15 +0300

    starting validation

--- mobilele/src/main/java/com/example/mobilele/mobilele/model/service/UserLoginServiceModel.java
+++ mobilele/src/main/java/com/example/mobilele/mobilele/model/service/UserLoginServiceModel.java
@@ -1,9 +1,16 @@
 package com.example.mobilele.mobilele.model.service;
 
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserLoginServiceModel {
 
+    @NotNull
+    @Size(min = 2)
     private String username;
+    @NotNull
+    @Size(min = 3)
     private String password;
 
     public String getUsername() {

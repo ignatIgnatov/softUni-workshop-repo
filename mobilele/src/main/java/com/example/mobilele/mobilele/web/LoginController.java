0b7da6792189e91c167256a7713fed783a4a4f78
Ignat Ignatov <ignatov_rs@abv.bg>
2023-06-27 17:35:46 +0300

    validation completed

--- mobilele/src/main/java/com/example/mobilele/mobilele/web/LoginController.java
+++ mobilele/src/main/java/com/example/mobilele/mobilele/web/LoginController.java
@@ -5,27 +5,49 @@ import com.example.mobilele.mobilele.security.CurrentUser;
 import com.example.mobilele.mobilele.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
 
-   private final UserService userService;
+    private final UserService userService;
 
-   @Autowired
+    @Autowired
     public LoginController(CurrentUser currentUser, UserService userService) {
-       this.userService = userService;
-   }
+        this.userService = userService;
+    }
+
+
+    @ModelAttribute("userModel")
+    public UserLoginServiceModel userModel() {
+        return new UserLoginServiceModel();
+    }
 
     @PostMapping("/users/login")
-    public String login(UserLoginServiceModel model) {
+    public String login(@Valid @ModelAttribute UserLoginServiceModel userModel,
+                        BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes) {
 
-    if (userService.authenticate(model.getUsername(), model.getPassword())) {
-        userService.loginUser(model.getUsername());
-        return "redirect:/";
-    }
-    return "redirect:/users/login";
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userModel", userModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userModel", bindingResult);
+            return "redirect:/users/login";
+        }
+
+        if (userService.authenticate(userModel.getUsername(), userModel.getPassword())) {
+            userService.loginUser(userModel.getUsername());
+            return "redirect:/";
+        } else {
+            redirectAttributes.addFlashAttribute("userModel", userModel);
+            redirectAttributes.addFlashAttribute("notFound", true);
+            return "redirect:/users/login";
+        }
     }
 
     @GetMapping("/users/login")
@@ -35,7 +57,7 @@ public class LoginController {
 
     @PostMapping("/users/logout")
     public String logout() {
-       userService.logoutCurrentUser();
+        userService.logoutCurrentUser();
         return "redirect:/";
     }
 }

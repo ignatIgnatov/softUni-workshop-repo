7aad861a3bac39d9676075e67117453747929ada
Ignat Ignatov <ignatov_rs@abv.bg>
2023-07-30 20:26:19 +0300

    app.js fetch post

--- books/src/main/resources/static/app.js
+++ books/src/main/resources/static/app.js
@@ -1,39 +1,79 @@
+let title = document.getElementById("title");
+let authorName = document.getElementById("author");
+let isbn = document.getElementById("isbn");
 
-$('#loadBooks').click(() => {
-    reloadBooks();
-})
+$("#loadBooks").click(() => {
+  reloadBooks();
+});
 
-function reloadBooks() {
+$("#add-btn").click(() => {
+  addBook();
+});
 
-    $("#authors-container").empty();
+function addBook() {
+  let temp = {
+    title: title.value,
+    isbn: isbn.value,
+    author: {
+      name: authorName.value,
+    },
+  };
 
-    fetch("http://localhost:8080/books")
-        .then(response => response.json())
-        .then(json => json.forEach(book => {
-            let tableRow = '<tr>' +
+  const httpHandlers = {
+    method: "POST",
+    body: JSON.stringify(temp),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
 
-                '<td>' + book.title + '</td>' +
-                '<td>' + book.author.name + '</td>' +
-                '<td>' + book.isbn + '</td>' +
-                '<td>' +
-                    '<button class="edit-btn" data-book-id="' + book.id + '">Edit</button>' +
-                    '<button class="delete-btn" data-book-id="' + book.id + '">Delete</button>' +
-                '</td>'
+  fetch("http://localhost:8080/books", httpHandlers)
+    .then(() => reloadBooks())
+    .catch((err) => console.error(err));
 
-            '</tr>'
-
-            $("#authors-container").append(tableRow)
-        }));
+  title.value = "";
+  authorName.value = "";
+  isbn.value = "";
 }
 
-$('body').on('click', 'button.delete-btn', function() {
+function reloadBooks() {
+  $("#authors-container").empty();
 
-    let bookId = $(this).data('book-id');
+  fetch("http://localhost:8080/books")
+    .then((response) => response.json())
+    .then((json) =>
+      json.forEach((book) => {
+        let tableRow =
+          "<tr>" +
+          "<td>" +
+          book.title +
+          "</td>" +
+          "<td>" +
+          book.author.name +
+          "</td>" +
+          "<td>" +
+          book.isbn +
+          "</td>" +
+          "<td>" +
+          '<button class="edit-btn" data-book-id="' +
+          book.id +
+          '">Edit</button>' +
+          '<button class="delete-btn" data-book-id="' +
+          book.id +
+          '">Delete</button>' +
+          "</td>";
 
-    fetch('http://localhost:8080/books/' + bookId,
-         {
-             method: 'DELETE'
-         })
-             .then(() => reloadBooks())
+        ("</tr>");
 
+        $("#authors-container").append(tableRow);
+      })
+    );
+}
+
+$("body").on("click", "button.delete-btn", function () {
+  let bookId = $(this).data("book-id");
+
+  fetch("http://localhost:8080/books/" + bookId, {
+    method: "DELETE",
+  }).then(() => reloadBooks());
 });

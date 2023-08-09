3776e844e6ed34fc559494beaff7962dcefe74b0
Ignat Ignatov <ignatov_rs@abv.bg>
2023-08-09 10:07:43 +0300

    Schedules demo.

---
+++ schedules/schedules/src/main/java/com/example/schedules/SchedulesApplication.java
@@ -0,0 +1,15 @@
+package com.example.schedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SchedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulesApplication.class, args);
+	}
+
+}
